d800c9de9c5ba4461d507e505d32beeadb137aaa
flux0r <flux0r@brettmingalls.com>
2013-03-19 16:07:20 -0400

    q1

--- l1/a.R
+++ l1/a.R
@@ -1,2 +1,21 @@
+omit.inf <- function(xs) {
+	return(xs[is.finite(xs)])
+}
+
+countLessThan <- function(x, ys) {
+	return(sum(na.omit(ys) <= x))
+}
+
+"%.%" <- function(f, g) {
+	return(function(...) {
+		return(f(g(...)))
+	})
+}
+
+finiteMax <- max %.% omit.inf %.% na.omit
+
 ckm <- read.csv("ckm.csv")
 
+adopters <- sapply(1:max(omit.inf(ckm$adoption_date)), function(x) {
+		return(countLessThan(x, ckm$adoption_date))
+	})
