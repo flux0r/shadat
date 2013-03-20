d9e99af9ef115c3d119b05d2909e1a95945c3cef
flux0r <flux0r@brettmingalls.com>
2013-03-19 21:49:31 -0400

    Finish lecture one computing exercise.

--- l1/a.R
+++ l1/a.R
@@ -14,8 +14,47 @@ countLessThan <- function(x, ys) {
 
 finiteMax <- max %.% omit.inf %.% na.omit
 
+pdf(file="g.pdf")
+
+# 4 a
 ckm <- read.csv("ckm.csv")
 
-adopters <- sapply(1:max(omit.inf(ckm$adoption_date)), function(x) {
+# 4 b
+# Adopters is a vector containing the number of people who had adopted by a
+# given date with the date given by the index of the vector. It ends at 17
+# because that is the maximum finite date in the data set variable.
+adopters <- sapply(1:17, function(x) {
 		return(countLessThan(x, ckm$adoption_date))
 	})
+
+# 4 c
+new <- diff(c(0, adopters))
+adopters <- c(0, adopters[1:length(new) - 1])
+p <- plot(adopters, new)
+
+# 4 d
+fit <- lm(new ~ adopters)
+abline(fit, col=hsv(0, 1, 0, 0.4))
+
+# 4 d
+#
+# The first two bandwidths suggest monotonicity, while the third does not.
+lines(ksmooth(adopters, new, "normal", bandwidth=200),
+	col=hsv(0.65, 1, 0.5, 0.8))
+lines(ksmooth(adopters, new, "normal", bandwidth=80),
+	col=hsv(0.90, 0.6, 0.5, 0.8))
+lines(ksmooth(adopters, new, "normal", bandwidth=25),
+	col=hsv(0.45, 0.6, 0.5, 0.8))
+
+# 4 e
+#
+# The residuals do not appear to be independent of the predictor.
+p.diag <- plot(adopters, residuals(fit))
+lines(ksmooth(adopters, residuals(fit), "normal", bandwidth=200),
+	col=hsv(0.65, 1, 0.5, 0.8))
+lines(ksmooth(adopters, residuals(fit), "normal", bandwidth=80),
+	col=hsv(0.90, 0.6, 0.5, 0.8))
+lines(ksmooth(adopters, residuals(fit), "normal", bandwidth=25),
+	col=hsv(0.45, 0.6, 0.5, 0.8))
+
+dev.off()
